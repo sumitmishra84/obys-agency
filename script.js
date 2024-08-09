dda16b8d85bc81e14edeb026d29f7317cb0502ac
Sumit Mishra <sm0141136@gmail.com>
2024-08-09 12:10:26 +0530

    Made loader successfully

---
+++ script.js
@@ -0,0 +1,43 @@
+var tl = gsap.timeline();
+tl.from(".line h1", {
+    y: 150,
+    stagger: 0.25,
+    duration: 0.6,
+    delay: 0.5
+});
+tl.from("#line-part1", {
+    opacity: 0,
+    onStart:function(){
+        var h5timer = document.querySelector("#line-part1 h5");
+        var grow = 0;
+        var interval = setInterval(function() {  
+            if (grow < 100) {
+                h5timer.innerHTML = grow++;
+            }else {
+                h5timer.innerHTML = grow;
+                clearInterval(interval);
+            }
+        },33);
+    },
+});
+tl.to(' .line h2',{
+    animationName:"anime",
+    opacity:1
+
+})
+tl.to("#loader", {
+    opacity: 0,
+    duration: 0.4,
+    delay: 4
+});
+
+tl.from("#page1",{
+    delay:0.2,
+    y:1600,
+    opacity:0,
+    duration:0.5,
+    ease:Power4
+})
+tl.to("#loader",{
+    display:"none"
+})
